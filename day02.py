23b101860a4f7ba5895f851a56cad246f5ec7fd9
JHKFranciscus <bigtimejr@naver.com>
2026-07-07 10:18:19 +0900

    Day 2: bariables and input

---
+++ day02.py
@@ -0,0 +1,46 @@
+# #STEP5. 변수 여러 개
+# name="Franciscus"
+# age=28
+
+# print(name)
+# print(age)
+
+# #STEP6.숫자는 따옴표가 없다
+
+# height = 175
+
+# print(height)
+
+# #STEP7. 문자열 더하기
+
+# first_name = "Kim"
+# last_name = "Minsu"
+
+# print(first_name + last_name)
+
+# #STEP8. 숫자 더히기
+
+# a = 10
+# b = 20
+
+#STEP 9. 문자열과 숫자는 다르다
+
+# print(a + b)
+
+# print("10" + "20")
+
+# print(10 + 20)
+
+# #STEP10. input과 형변환
+
+# age = int(input("나이를 입력하세요: " ))
+
+# print(age + 1)
+
+#STEP 11. 오늘의 실습(
+
+name = input("이름을 입력하세요: ")
+age = int(input("나이를 입력하세요: "))
+
+print("안녕하세요", name)
+print("내년 나이는", age +1, "살입니다.")
